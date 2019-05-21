3254745818a72f13def830fac8ec5294186aa798
Maurice Diesendruck <moglobal@gmail.com>
2019-05-21 13:38:47 -0500

    added to data.py to output cluster centers

--- mv_sp_privacy/src/data.py
+++ mv_sp_privacy/src/data.py
@@ -28,16 +28,16 @@ def generate_data_mixture_of_Gaussians_01(N_private, D):
     centers_high = 1 - 4. * sigma_data
     mus = np.random.uniform(low=centers_low, high=centers_high, size=(C, D))
 
-    ws = np.array([1.0 / n for n in range(1, C+1)])
-    ws /= np.sum(ws)
-    memberships = np.random.choice(range(C), size=N_private, p=ws)
+    weights = np.array([1.0 / n for n in range(1, C+1)])
+    weights /= np.sum(weights)
+    memberships = np.random.choice(range(C), size=N_private, p=weights)
     X_private = []
     for n in range(N_private):
         mu = mus[memberships[n]]
         sample = np.random.normal(loc=mu, scale=sigma_data)
         X_private.append(sample)
     X_private = np.array(X_private)
-    return X_private
+    return X_private, mus, weights, sigma_data
 
 
 def main(args_dict):
@@ -48,12 +48,19 @@ def main(args_dict):
     D = args_dict['D']
 
     # Generate the private data
-    X_private = generate_data_mixture_of_Gaussians_01(N, D)
+    (X_private,
+     mus,
+     weights,
+     sigma_data) = generate_data_mixture_of_Gaussians_01(N, D)
 
     # Save the generated data
     dataset = 'mixture_of_Gaussians'
     path_save = '../data/%s_N%d_D%d' % (dataset, N, D)
-    np.savez(path_save + '.npz', X_private=X_private)
+    np.savez(path_save + '.npz',
+             X_private=X_private,
+             MUS=mus,
+             WEIGHTS=weights,
+             SIGMA_DATA=sigma_data)
     json_dump({'dataset': dataset, 'N': N, 'D': D}, path_save + '.json')
 
 
